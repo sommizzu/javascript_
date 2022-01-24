c9166e49e0d2aabed7119da351d79b2dd2c36d32
someee <79901950+sommizzu@users.noreply.github.com>
2022-01-24 12:01:20 +0000

    Initial commit

---
+++ script.js
@@ -0,0 +1,15 @@
+let color = "black"
+//let : 변수 선언 하는 키워드 
+//color :변수 이름 
+// = : 오른쪽에 있는 값을 왼쪽에다 집어 넣는다!
+console.log(color)
+//console 에다가 log를 찍겠다 라는 명령어 / 변수 color부름
+//color에 balck이 들어왔으니까 black이 찍힘
+
+let color2 = "pink"
+console.log(color2)
+
+//let을 안쓰면 기존에 있는 것 가져다가 쓰는 것
+
+//만약 let color = "black"
+//변수안에는 어떠한 값을 넣을수 있고, 변수만 부르면 해당 값이 나온다
