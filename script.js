fe5cf9f1aa25e8885d7823b84f30ddba8983c5be
someee <79901950+sommizzu@users.noreply.github.com>
2022-01-24 12:24:29 +0000

    string 자료형 (문자)

--- script.js
+++ script.js
@@ -1,8 +1,7 @@
-var password = "pink"
-password = "black"
+let password = "123"+1
 
 
 console.log(password)
 
-//var -> 업데이트 -> cosnt,let이 나옴
-//var쓰지 않는 것이 좋다!
+//값의 종류 구별
+//string 문장,단어,apsdjflkajdfla kjdfl 같은것도 string,알파벳 하나, 123이런것들도 ""큰 따음표나 ''작은 따음표 안에 들어가 있는것들은 모두 문자로 취급함
