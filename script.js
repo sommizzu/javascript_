872390f2cf606dd16a4f642ca8d27204b0042936
someee <79901950+sommizzu@users.noreply.github.com>
2022-01-24 12:28:28 +0000

    같은것 같은데 값이 다르게 나온 이유

--- script.js
+++ script.js
@@ -1,5 +1,7 @@
-let password = 1 //password값 1임
-password = password+2 //1+3된 값을 다시 password에 넣음!
+let password = 1 
+ password+2 // 이순간에는 2를 더하지만 어디에도 가지 못하고 소멸되버리기 때문에 1이 결과값임
+
+ //항상 = 를 사용해서 할당해줘야함!(다시 password에 담아줘야함)
 
 
 console.log(password)
