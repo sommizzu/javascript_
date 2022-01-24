be7948ce5b2dd2b6be39e13be74d6669caf31fd0
someee <79901950+sommizzu@users.noreply.github.com>
2022-01-24 12:25:21 +0000

    숫자형 데이터타입

--- script.js
+++ script.js
@@ -1,7 +1,5 @@
-let password = "123"+1
+let password = -123.2+1
 
 
 console.log(password)
 
-//값의 종류 구별
-//string 문장,단어,apsdjflkajdfla kjdfl 같은것도 string,알파벳 하나, 123이런것들도 ""큰 따음표나 ''작은 따음표 안에 들어가 있는것들은 모두 문자로 취급함
