5cc9e8566b898fe65f090cb963bbc619df1e9b9d
someee <79901950+sommizzu@users.noreply.github.com>
2022-01-24 12:45:48 +0000

    출력값 예측해보기

--- script.js
+++ script.js
@@ -1,10 +1,35 @@
-let a = 1 
-let b = 2
 
-console.log(a,b)
+let d =1 + 2 * 3 
+let e =(1 + 3) ** 2   
+let f =1 / 0    
+let g =6 % 2    
+let h =7.5 % 2    
+let i =5 == 5   
+let j =5 === 5 
+let k =5 == "5" 
+let l =5 === "5"
+let m =5 != 5.0 
+let n =5 !== 5.0 
+let o ="true" === true 
+let p =5 <= 5.0
+let q =5 >= 5 
+let r =true || true 
 
-let c = a
-a = b
-b = c
+console.log(d)
+console.log(e)
+console.log(f)
+console.log(g)
+console.log(h)
+console.log(i)
+console.log(j)
+console.log(k)
+console.log(l)
+console.log(m)
+console.log(n)
+console.log(o)
+console.log(p)
+console.log(q)
+console.log(r)
 
-console.log(a,b)
+
+
